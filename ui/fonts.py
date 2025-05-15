1909b1d627c1063c9f4273b1e66125817f449056
Ay0ko <128031560+Ay0ko@users.noreply.github.com>
2025-05-15 09:26:29 +0200

    changement taile font

--- ui/fonts.py
+++ ui/fonts.py
@@ -3,7 +3,7 @@ import pygame
 def init_fonts():
     # Polices
     font = pygame.font.Font(None, 80)  # Grande police pour le titre
-    button_font = pygame.font.Font(None, 40)  # Police pour les boutons
+    button_font = pygame.font.Font(None, 35)  # Police pour les boutons
     small_font = pygame.font.Font(None, 30)  # Police plus petite pour les textes secondaires
     
     return {
